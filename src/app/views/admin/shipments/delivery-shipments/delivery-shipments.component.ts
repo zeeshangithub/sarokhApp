fc6856a0da395b4fe7b05ad9b2d23615a312522f
hamid913 <hamidshah913@gmail.com>
2020-04-07 16:29:11 +0500

    delivery module added

---
+++ src/app/views/admin/shipments/delivery-shipments/delivery-shipments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-shipments',
+  templateUrl: './delivery-shipments.component.html',
+  styleUrls: ['./delivery-shipments.component.css']
+})
+export class DeliveryShipmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
