278f03ac472ad1f5cdf6e830b61c22cf13d4bfc3
hamid913 <hamidshah913@gmail.com>
2020-04-07 16:12:40 +0500

    module added

---
+++ src/app/views/admin/shipments/pickup-shipments/pickup-shipments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pickup-shipments',
+  templateUrl: './pickup-shipments.component.html',
+  styleUrls: ['./pickup-shipments.component.css']
+})
+export class PickupShipmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
