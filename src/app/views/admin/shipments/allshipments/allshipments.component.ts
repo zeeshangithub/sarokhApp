829a2a99bc14785d2934306efa38cca915a9f8bb
hamid913 <hamidshah913@gmail.com>
2020-06-21 00:59:09 +0500

    detail page added

--- src/app/views/admin/shipments/allshipments/allshipments.component.ts
+++ src/app/views/admin/shipments/allshipments/allshipments.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../../../services/admin.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-allshipments',
   templateUrl: './allshipments.component.html',
@@ -10,12 +10,14 @@ export class AllshipmentsComponent implements OnInit {
 
   public allShipments;
 
-  constructor(private AllShipments: AdminService) { }
+  constructor(private AllShipments: AdminService, private router: Router) { }
 
   ngOnInit(): void {
     this.getAllShipments();
   }
-
+  viewOrder(id) {
+    this.router.navigate(['shipment/vieworder', id]);
+  }
   getAllShipments() {
     this.AllShipments.fetchAllShipments().subscribe(res => {
       this.allShipments = res.data
